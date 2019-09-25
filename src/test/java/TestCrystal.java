f7d07e13eb05ea674f47929285f93ed041601cef
Armond Sarkisian <armond.sarkisian@gmail.com>
2019-09-25 08:15:45 -0700

    updated

--- src/test/java/TestCrystal.java
+++ src/test/java/TestCrystal.java
@@ -9,11 +9,27 @@ public class TestCrystal extends CFramework {
 
     public TestCrystal() { }
 
-    @Test
-    public static void test_01() {
+	// (Invoke exception as expected result: expectedExceptions=RuntimeException.class)
+	@Test (enabled=TableTest.IS_RUN_TEST_NEWS_RIGHT_RAIL, invocationCount=1, priority=1, alwaysRun=true, successPercentage=100, timeOut=1000*60*5, singleThreaded=true, description=TableTest.DESCRIPTION_TEST_NEWS_RIGHT_RAIL)
+	public void test_01 () throws InterruptedException {
+		
+		// is this test case going to run?
+		if(!Table.incoming_isRunMethod) {
+			
+			// disable this test case
+		    CFramework.skipTest("Warning: Test case has been disabled!");
+		}
+		
+		// instantiate news object
+		Test test = PageFactory.initElements(Browser.getDriver(), Test.class);
 
-        System.out.println("Test 1");
+		Reporting.display_testCaseProgress(Reporting.TestProgressState.START_TEST, new Object(){}.getClass().getEnclosingMethod().getName().toString());
 
-        return;
-    }
+		// test news right rail functions
+		test.test_crystal(); 
+
+		Reporting.display_testCaseProgress(Reporting.TestProgressState.END_TEST, new Object(){}.getClass().getEnclosingMethod().getName().toString());
+
+		return;
+	}
 }
